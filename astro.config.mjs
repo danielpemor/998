8278fb67ed0a8638b0ff873ec22e6a493918fe70
Daniel <dpm__10@hotmail.com>
2025-09-19 11:54:08 -0600

    w

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/server'; // 👈 importante
 
 export default defineConfig({
   integrations: [
@@ -9,4 +9,5 @@ export default defineConfig({
     tailwind(),
   ],
   output: 'server',
+  adapter: vercel(), // 👈 agrega el adapter
 });
