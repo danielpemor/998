040645e096f104e8201916cb43895739b9781905
Daniel <dpm__10@hotmail.com>
2025-09-19 12:02:26 -0600

    w

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel'; 
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions"; 
 
 export default defineConfig({
   integrations: [
     react(),
     tailwind(),
   ],
-  output: 'server',
-  adapter: vercel(),
+  output: "server", // o "hybrid" estático + SSR
+  adapter: netlify(),
 });
